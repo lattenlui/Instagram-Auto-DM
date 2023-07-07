aba72f30fa81b8cab4f1d12b6cd2a3256ab0072f
Sahib <100060676+Blackysh@users.noreply.github.com>
2023-07-07 13:00:28 +0530

    asd

--- datascraper.py
+++ datascraper.py
@@ -1,13 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import pandas as pd 
+from selenium import webdriver 
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
 
 link = 'https://hiftie.xyz/'
 
 
-
+options = ChromeOptions()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options=options)
 response = requests.get(link)
-soup = BeautifulSoup(response.content, 'html.parser')
+soup = BeautifulSoup(response.content, 'lxml')
+driver.get(link)
+html = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
+print(html)
 
 # Get Contact Information and remove duplticates
 def getContact(html):
