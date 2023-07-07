b7d07a241e5a5c0ab79d942fd99960366126474c
Sahib <100060676+Blackysh@users.noreply.github.com>
2023-07-07 11:47:51 +0530

    t

--- datascraper.py
+++ datascraper.py
@@ -2,15 +2,29 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+link = 'https://hiftie.xyz/'
 
-r = requests.get('https://arqies.com')
-soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.prettify())
 
-email_pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
 
-# Get emails and remove duplticates
-emails = re.findall(email_pattern, str(soup))
-emails = [*set(emails)]
+response = requests.get(link)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-print(emails)
+# Get Contact Information and remove duplticates
+def getContact(html):
+
+    def getEmail():
+        email_pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,4}"
+        emails = re.findall(email_pattern, str(html))
+        emails = [*set(emails)]
+        return emails
+
+
+    Email = getEmail() 
+    # Phone = getPhone()
+    # Phone = [*set(Phone)]
+
+    return Email #, Phone
+
+Contact = getContact(soup)
+
+print(Contact)
