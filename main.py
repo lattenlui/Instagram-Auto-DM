9464796f8363f432debc5a8278e72f6885bf01bc
Sahib <100060676+Blackysh@users.noreply.github.com>
2023-07-21 19:19:33 +0530

    asd

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import API
-import ask_gpt
-import mailHandler
-import instagramScraper
+from instagramScraper import start
+from multiprocessing import Process
 
+def flask():
+    import instagramReply
+
+if __name__ == "__main__":
+    p1 = Process(target=start)
+    p1.start()
+    p2 = Process(target=flask)
+    p2.start()
+    p1.join()
+    p2.join()
