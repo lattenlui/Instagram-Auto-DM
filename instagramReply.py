743a428a1bd5b0106df24ac637161dc932b60807
Sahib <100060676+Blackysh@users.noreply.github.com>
2023-07-23 13:33:55 +0530

    done i think

--- instagramReply.py
+++ instagramReply.py
@@ -11,7 +11,10 @@ port = os.getenv('PORT', 3000)
 def return_response():
     print(request.json)
     
-    data = json.load(request.json)
+    data = json.load(request.json)  
+
+    if data == None:
+        return Response(status=200)
 
     # Extract id and msg || REMOVE [0] if doesn't work
     common = data['entry'][0]['messaging'][0]
@@ -21,6 +24,14 @@ def return_response():
     os.system('python3 reply.py ' + uid + ' "' + msg + '"')
 
     return Response(status=200)
+
+@app.route('/f4c3b00kw3bh00k', methods=['GET'])
+def webhook_verify():
+    verify_token = 'msg-reciever'
+    if request.args.get('hub.verify_token') == verify_token:
+        return request.args.get('hub.challenge')
+    return "Wrong verify token"
+
 
 app.run(host='0.0.0.0', port=port)
 
